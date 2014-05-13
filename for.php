3e567e2c424e057ce3ef010eda073a3bc5753e39
Alex Zuniga <guest@Guests-MacBook-Air.local>
2014-05-13 11:43:38 -0500

    Error response for false numeric values

--- for.php
+++ for.php
@@ -3,20 +3,28 @@
 //Control Structure exercise.
 //Alex Zuniga
 
-// Prompt user for a starting number and ending number, 
+// Prompt user for a starting number
 
-fwrite(STDOUT, "Enter a Starting Number please.\n");
-
+do {
+	fwrite(STDOUT, "Enter a Starting Number please.\n");
 	$starting = trim(fgets(STDIN));
+} while (!is_numeric($starting));
 
-fwrite(STDOUT, "Now enter an Ending number.\n");
 
+//Entering second number
+do {
+	fwrite(STDOUT, "Enter a Ending Number please.\n");
 	$ending = trim(fgets(STDIN));
-	
-//then display all the numbers from the starting to ending using a for loop.
+} while (!is_numeric($ending));
+
+
+// setting the increment
 fwrite(STDOUT, "Excellent! Now choose an number to count by. ");
 
 $increment = trim(fgets(STDIN));
+if ($increment == "") {
+	$increment = 1;
+}
 
 if ($starting < $ending) {
 
@@ -33,11 +41,5 @@ if ($starting < $ending) {
 
 
 
-// Refactor to allow user to choose increment. (count by 1, 2, 10, ...)
-
-
-// Default increment to 1 if no input.
-
-// Make sure you are only allowing users to pass in numbers. Give an error message is both passed arguments are not numeric. See php.net/is_numeric.
 
 ?>
