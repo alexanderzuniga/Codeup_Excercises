e3fbba3acdfe20129e594bab38af756711fff15f
Alex Zuniga <guest@Guests-MacBook-Air.local>
2014-05-15 11:46:41 -0500

    Diplay error if NOT numeric

--- arithmatic.php
+++ arithmatic.php
@@ -1,57 +1,61 @@
 <?php
 
 
-// function add($a, $b) {
-//     echo $a + $b . PHP_EOL;
-// }
-
-// function subtract($a, $b) {
-//     echo $a - $b . PHP_EOL;
-// }
-
-// function multiply($a, $b) {
-//     echo $a * $b . PHP_EOL;
-// }
-
-// function divide($a, $b) {
-//     echo $a / $b . PHP_EOL;
-// }
-
-// function remain($a,$b) {
-// 	echo $a % $b . PHP_EOL;
-// }
-
-// function compare($a,$b, $strict = true) {
-// 	if($strict === true) {
-// 		echo $a === $b ? 'TRUE' : 'FALSE';
-// 	    } else{ 
-// 			echo $a == $b ? 'TRUE' : 'FALSE';
-// 		}
-// 		echo PHP_EOL;
-// }
-
-// compare('Omar','omar', false);
-
-// add(1,1);
-// subtract(10,5);
-// multiply(4,3);
-// divide(16,4);
-// remain(15,3);
+function add($a, $b) {
+  if (is_numeric($a) && is_numeric($b)) {
+        echo $a + $b . PHP_EOL;
+    } else {
+        echo "ERROR: Both arguments must be numbers\n";
+    }
+}  
 
 
-
-function inspect($variable = null, $dump = true)
-{	if ($dump === true) {
-		var_dump($variable);
-	} else {
-		print_r($variable);
-	}
+function subtract($a, $b) {
+  if (is_numeric($a) && is_numeric($b)) {
+        echo $a - $b . PHP_EOL;
+    } else {
+        echo "ERROR: Both arguments must be numbers\n";
+    }
 }
 
-$heroes = (['batman', 'superman', 'bartman']);
-inspect($heroes);
+function multiply($a, $b) {
+    if (is_numeric($a) && is_numeric($b)) {
+        echo $a * $b . PHP_EOL;
+    } else {
+        echo "ERROR: Both arguments must be numbers\n";
+    }
+}
 
-print_r($heroes);
+function divide($a, $b) {
+    if (is_numeric($a) && is_numeric($b)) {
+        echo $a / $b . PHP_EOL;
+    } else {
+        echo "ERROR: Both arguments must be numbers\n";
+    }
+}
 
+function remain($a,$b) {
+	if (is_numeric($a) && is_numeric($b)) {
+        echo $a * $b . PHP_EOL;
+    } else {
+        echo "ERROR: Both arguments must be numbers\n";
+    }
+}
+
+function compare($a,$b, $strict = true) {
+	if($strict === true) {
+		echo $a === $b ? 'TRUE' : 'FALSE' . PHP_EOL;
+	    } else{ 
+			echo $a == $b ? 'TRUE' : 'FALSE' . PHP_EOL;
+		}
+		
+}
+
+compare('Omar','omar', false);
+add(1,'batman');
+subtract('elf',5);
+multiply('super',3);
+divide('line',4);
+remain(15,'three');
 
 ?>
