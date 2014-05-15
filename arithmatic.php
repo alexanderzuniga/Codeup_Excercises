d9db3e79deb8e67740c32b5b64beff4ad5f97f6b
Alex Zuniga <guest@Guests-MacBook-Air.local>
2014-05-15 11:11:12 -0500

    various arithmatic added on

--- arithmatic.php
+++ arithmatic.php
@@ -1,29 +1,57 @@
 <?php
 
 
-function add($a, $b) {
-    echo $a + $b . PHP_EOL;
+// function add($a, $b) {
+//     echo $a + $b . PHP_EOL;
+// }
+
+// function subtract($a, $b) {
+//     echo $a - $b . PHP_EOL;
+// }
+
+// function multiply($a, $b) {
+//     echo $a * $b . PHP_EOL;
+// }
+
+// function divide($a, $b) {
+//     echo $a / $b . PHP_EOL;
+// }
+
+// function remain($a,$b) {
+// 	echo $a % $b . PHP_EOL;
+// }
+
+// function compare($a,$b, $strict = true) {
+// 	if($strict === true) {
+// 		echo $a === $b ? 'TRUE' : 'FALSE';
+// 	    } else{ 
+// 			echo $a == $b ? 'TRUE' : 'FALSE';
+// 		}
+// 		echo PHP_EOL;
+// }
+
+// compare('Omar','omar', false);
+
+// add(1,1);
+// subtract(10,5);
+// multiply(4,3);
+// divide(16,4);
+// remain(15,3);
+
+
+
+function inspect($variable = null, $dump = true)
+{	if ($dump === true) {
+		var_dump($variable);
+	} else {
+		print_r($variable);
+	}
 }
 
-function subtract($a, $b) {
-    echo $a - $b . PHP_EOL;
-}
+$heroes = (['batman', 'superman', 'bartman']);
+inspect($heroes);
 
-function multiply($a, $b) {
-    echo $a * $b . PHP_EOL;
-}
+print_r($heroes);
 
-function divide($a, $b) {
-    echo $a / $b . PHP_EOL;
-}
 
-function remain($a,$b) {
-	echo $a % $b . PHP_EOL;
-}
-
-add(1,1);
-subtract(10,5);
-multiply(4,3);
-divide(16,4);
-remain(15,3);
 ?>
