4ff7bb0d366a47cd687d5726bf13d0d83ec2a169
Alex Zuniga <guest@Guests-MacBook-Air.local>
2014-05-16 09:30:09 -0500

    aligning text

--- arithmatic.php
+++ arithmatic.php
@@ -2,68 +2,73 @@
 
 function error($a, $b, $is_not_numeric = true) {
 	if ($is_not_numeric) {
-        echo "ERROR: {$a} and/or {$b} are not numeric values, enter numbers.\n";
+        return 'ERROR: $a and/or $b are not numeric values, enter numbers.' . PHP_EOL;
     } else {
-        echo "ERROR: Please enter denominator < or > 0. \n";
+        return 'ERROR: Please enter denominator < or > 0.' . PHP_EOL;
 	}
 }
 
 function add($a, $b) {
   if (is_numeric($a) && is_numeric($b)) {
-        echo $a + $b . PHP_EOL;
+        return $a + $b;
     } else {
-    	error($a, $b);
+    	return error($a, $b);
     }
 }
-    
+
+// $sum = add(2, 'banana');
+// echo $sum . PHP_EOL; 
  
-//add(1, "batman");
 
 function subtract($a, $b) {
   if (is_numeric($a) && is_numeric($b)) {
-        echo $a - $b . PHP_EOL;
+        return $a - $b;
     } else {
-    	error($a, $b);
-    	}
+    	return error($a, $b);
+    }
 
 }
-//subtract(4, 'batman');
+// $sub_answer = subtract(4, 'banana');
+// echo $sub_answer . PHP_EOL;
 
 function mult($a, $b) {
     if (is_numeric($a) && is_numeric($b)) {
-        echo $a * $b . PHP_EOL;
+        return $a * $b;
     } else {
-    	error($a, $b);
+    	return error($a, $b);
     	}
 }
 
-//mult(4, 'bartman');
+// $multi_answer = mult(4, 3);
+// echo $multi_answer . PHP_EOL;
 
 function remain($a,$b) {
 	if (is_numeric($a) && is_numeric($b)) {
-        echo $a % $b . PHP_EOL;
+        return $a % $b;
     } else {
-    	error($a, $b);
+    	return error($a, $b);
     	}
 }
 
-//remain(12,'batman');
+// $remainder = remain(5,2);
+// echo $remainder . PHP_EOL;
 
 
 function divide($a, $b) {
      if (is_numeric($a) && is_numeric($b)) {
      	if ($b == 0) { 
-	    	error($a, $b, 0);
+	    	return error($a, $b, false);
 	    } else {
-			echo $a / $b . PHP_EOL;
+			return $a / $b . PHP_EOL;
 		}
 	} else {
 		error($a, $b);
 	}
 }
+$div_answer = divide(4, 0);
+echo $div_answer . PHP_EOL;
 
-divide(4, 0);
-divide('a', 1);
+
 
 
 ?>
